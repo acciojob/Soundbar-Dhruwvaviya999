14c0525624836504a961f73178de4c352ee2aa63
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-22 15:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let applause = new Audio("sounds/tada.mp3");
+
+let btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		applause.play();
+	})
+});

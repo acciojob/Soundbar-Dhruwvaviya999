13b942c269a991cd875784f382642262b781f28d
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-03 10:48:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,25 @@
-//your JS code here. If required.
 
-let applause = new Audio("sounds/applause.mp3");
-let boo = new Audio("sounds/boo.mp3");
-let gasp = new Audio("sounds/gasp.mp3");
-let tada = new Audio("sounds/tada.mp3");
-let victory = new Audio("sounds/victory.mp3");
-let wrong = new Audio("sounds/wrong.mp3");
+let sounds = ["applause.mp3", "boo.mp3", "gasp.mp3", "tada.mp3", "victory.mp3", "wrong.mp3"]; 
+let buttonsContainer = document.querySelector(".buttons");
+const audioPlayer = document.querySelector("#audio-player");
 
-document.querySelector(".applause").addEventListener("click", function() {
-    applause.play();
-});
-document.querySelector(".boo").addEventListener("click", function() {
-    boo.play();
-});
-document.querySelector(".gasp").addEventListener("click", function() {
-    gasp.play();
-});
-document.querySelector(".tada").addEventListener("click", function() {
-    tada.play();
-});
-document.querySelector(".victory").addEventListener("click", function() {
-    victory.play();
-});
-document.querySelector(".wrong").addEventListener("click", function() {
-    wrong.play();
+ sounds.forEach(sound => {
+	const btn = document.createElement("button");
+	btn.className = "btn";
+	btn.textContent = sound.replace(".mp3", "");
+	btn.addEventListener("click", () => {
+		audioPlayer.src = `sounds/${sound}`;
+		audioPlayer.play();
+	});
+	buttonsContainer.appendChild(btn);
 });
 
-document.querySelector(".stop").addEventListener("click", function() {
-    applause.pause();
-	boo.pause();
-	gasp.pause();
-	tada.pause();
-	victory.pause();
-	wrong.pause();
-    applause.currentTime = 0;
-    boo.currentTime = 0;
-    gasp.currentTime = 0;
-    tada.currentTime = 0;
-    victory.currentTime = 0;
-    wrong.currentTime = 0;
+const stopBtn = document.createElement("button");
+stopBtn.className = "stop btn";
+stopBtn.textContent = "Stop";
+stopBtn.addEventListener("click", ()=>{
+	audioPlayer.pause();
+	audioPlayer.currentTime = 0;
+})
+buttonsContainer.appendChild(stopBtn);
 
-	
-});
